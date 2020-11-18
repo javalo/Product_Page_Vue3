809d7524686ba3c04be68c1142ca2f25612d879f
javalo <lo.cheikhna@gmail.com>
2020-11-18 19:31:26 +0100

    select color product by mouseover

--- main.js
+++ main.js
@@ -14,11 +14,13 @@ const app = Vue.createApp({
         }
     }
 ,
-
-
     methods:{
         addToCart(){
             this.cart += 1 
-        } 
-    }
+        } ,
+        updateImage(variantImage){
+            this.image = variantImage
+        }
+    },
+
 })
