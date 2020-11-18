c558db3a5bbf2fa80bfa3e7e1cc5bef57f1e1e3f
javalo <lo.cheikhna@gmail.com>
2020-11-18 23:51:59 +0100

    communicationg Events

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart:0,
+            cart:[],
             premium1: true
         }
     }
 ,
     methods:{
-      
+        updateCart(id){
+            this.cart.push(id)
+        }
     }
 })
