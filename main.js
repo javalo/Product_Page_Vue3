82531f547a5e36ffe38368c31b56d2132a3982a7
javalo <lo.cheikhna@gmail.com>
2020-11-18 18:57:29 +0100

    add variants

--- main.js
+++ main.js
@@ -5,7 +5,11 @@ const app = Vue.createApp({
         product : 'Socks',
         image: './assets/images/socks_green.jpg',
         inventory: 11,
-        details : ['50% cotton', '30% wool', '20% polyester']
+        details : ['50% cotton', '30% wool', '20% polyester'],
+        variants : [
+            {id: 2234, color: 'green'},
+            {id: 2235, color: 'blue'}
+        ]
     }
  }
 
